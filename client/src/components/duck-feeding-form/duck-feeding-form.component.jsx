921a820e8f9d374367467a08ced4f8c790c26ed7
Leon <yarektenko@gmail.com>
2022-02-18 21:29:33 -0800

    Implement handleChange for the DuckFeedingForm component
    - Implemented handleChange to work with the given state
    - Provided basic implementation for handleSubmit to prevent default
      behaviour

--- client/src/components/duck-feeding-form/duck-feeding-form.component.jsx
+++ client/src/components/duck-feeding-form/duck-feeding-form.component.jsx
@@ -1,33 +1,46 @@
 import "./duck-feeding-form.styles.scss";
+import React from "react";
 
 // Component imports
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 
-const DuckFeedingForm = () => (
-  <div className="">
-    <h2>Add a new duck stat</h2>
-    <span>Enter your duck feeding information below</span>
+const DuckFeedingForm = () => {
+  const [duckFormData, setDuckFormData] = React.useState({
+    _id: "",
+    feedingTime: null,
+    foodType: "",
+    feedingLocation: "",
+    duckCount: 0,
+    foodAmountKg: 0,
+  });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(duckFormData);
+  };
 
-    <form
-      action=""
-      onSubmit={() => {
-        console.log("Test");
-      }}
-    >
-      <FormInput
-        type="text"
-        name=""
-        value=""
-        required
-        label=""
-        handleChange={() => {
-          console.log("Test");
-        }}
-      />
-      <CustomButton type="submit">Submit</CustomButton>
-    </form>
-  </div>
-);
+  const handleChange = (e) => {
+    setDuckFormData({ [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div className="">
+      <h2>Add a new duck stat</h2>
+      <span>Enter your duck feeding information below</span>
+
+      <form action="" onSubmit={handleSubmit}>
+        <FormInput
+          type="text"
+          name="foodType"
+          value={duckFormData.foodType}
+          required
+          label="Food Type"
+          handleChange={handleChange}
+        />
+        <CustomButton type="submit">Submit</CustomButton>
+      </form>
+    </div>
+  );
+};
 
 export default DuckFeedingForm;
