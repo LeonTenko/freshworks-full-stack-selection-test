6c5879e484f3bd4d3d0274e0b6edef9bafe62cee
Leon <yarektenko@gmail.com>
2022-02-19 12:09:25 -0800

    Add Date Picker to Form
    - Implemented MUI Date Picker
    - Made sure the state changes correctly
    - Tested POST work correctly and output correct data
    - Refactored code in form-input component and duck-feeding-form to
      be more generic

--- client/src/components/duck-feeding-form/duck-feeding-form.component.jsx
+++ client/src/components/duck-feeding-form/duck-feeding-form.component.jsx
@@ -1,6 +1,10 @@
 import "./duck-feeding-form.styles.scss";
 import React from "react";
 
+// MaterialUI imports
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+
 // Component imports
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
@@ -9,7 +13,6 @@ const DuckFeedingForm = ({ reRenderParent }) => {
   // For POST testing purposes
   const defaultValues = {
     _id: "",
-    feedingTime: Date.now(),
     feedingLocation: "test",
     duckCount: 1,
     foodAmountKg: 1,
@@ -17,8 +20,14 @@ const DuckFeedingForm = ({ reRenderParent }) => {
 
   const [duckFormData, setDuckFormData] = React.useState({
     foodType: "",
+    feedingTime: new Date(),
     ...defaultValues,
   });
+
+  // Just logging to see if the state is correct, remove later
+  React.useEffect(() => {
+    console.log(duckFormData);
+  }, [duckFormData]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -52,26 +61,46 @@ const DuckFeedingForm = ({ reRenderParent }) => {
   };
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
-    setDuckFormData({ [name]: value, ...defaultValues });
+    let name = "";
+    let value = "";
+
+    if (e.target) {
+      name = e.target.name;
+      value = e.target.value;
+    } else {
+      // Date picker works directly with value and not e.target
+      name = "feedingTime";
+      value = e;
+    }
+
+    setDuckFormData({ ...duckFormData, [name]: value });
   };
 
   return (
     <div className="">
-      <h2>Add a new duck stat</h2>
-      <span>Enter your duck feeding information below</span>
-
-      <form action="" onSubmit={handleSubmit}>
+      <Box component="form" onSubmit={handleSubmit}>
         <FormInput
           type="text"
           name="foodType"
           value={duckFormData.foodType}
           required
-          label="Food Type"
+          label="Food"
+          helperText="What food did you feed the ducks?"
           handleChange={handleChange}
         />
+        <FormInput
+          type="datePicker"
+          name="feedingTime"
+          value={duckFormData.feedingTime}
+          required
+          label="Feeding Time"
+          dateTimePicker
+          helperText="When did you feed the ducks?"
+          handleChange={handleChange}
+        />
+
         <CustomButton type="submit">Submit</CustomButton>
-      </form>
+      </Box>
     </div>
   );
 };
--- client/src/components/form-input/form-input.component.jsx
+++ client/src/components/form-input/form-input.component.jsx
@@ -1,15 +1,38 @@
 import "./form-input.styles.scss";
 
-const FormInput = ({ handleChange, label, ...otherProps }) => (
-  <div className="">
-    <input
-      onChange={handleChange}
-      type="text"
-      className="form-input"
-      {...otherProps}
-    />
-    {label ? <label className="">{label}</label> : null}
-  </div>
-);
+// MaterialUI imports
+import TextField from "@mui/material/TextField";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import DateTimePicker from "@mui/lab/DateTimePicker";
+
+const FormInput = ({ handleChange, type, ...otherProps }) => {
+  switch (type) {
+    case "datePicker":
+      return (
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <DateTimePicker
+            renderInput={(props) => (
+              <TextField
+                required={otherProps.required}
+                helperText={otherProps.helperText}
+                {...props}
+              />
+            )}
+            onChange={handleChange}
+            {...otherProps}
+          />
+        </LocalizationProvider>
+      );
+    default:
+      return (
+        <TextField
+          className="form-input"
+          onChange={handleChange}
+          {...otherProps}
+        />
+      );
+  }
+};
 
 export default FormInput;
