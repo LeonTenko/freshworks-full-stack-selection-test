db33765511145b8f415f49165c3b2db06ce4f2e1
Leon <yarektenko@gmail.com>
2022-02-19 17:50:50 -0800

    Provide Table Layout for Duck Feeding Entries
    - Used MaterialUI to put the server data into a table
    - The table implements pagination
    - The table has a sticky header
    - Provided formatting to appropriate table cells
    - Tested it to work with submit
    
    To-do:
    + Refactor code, split into more components where it makes sense (right
      now it's all in the Collection component)
    + Provide ID table cell

--- client/src/components/collection/collection.component.js
+++ client/src/components/collection/collection.component.js
@@ -11,11 +11,23 @@ import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
+import TableCell from "@mui/material/TableCell";
+import TableRow from "@mui/material/TableRow";
 import TablePagination from "@mui/material/TablePagination";
 
 const Collection = () => {
-  const [duckData, setDuckData] = React.useState(null);
+  const [duckData, setDuckData] = React.useState([]);
   const [reRender, setReRender] = React.useState(false);
+
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const [page, setPage] = React.useState(0);
+  const handleChangePage = (e, newPage) => {
+    setPage(newPage);
+  };
+  const handleChangeRowsPerPage = (e) => {
+    setRowsPerPage(e.target.value);
+    setPage(0);
+  };
 
   // Tritter re-render every time someone adds a new
   // duck feeding entry
@@ -47,22 +59,41 @@ const Collection = () => {
         {duckData ? (
           <TableContainer sx={{ maxHeight: "70vh" }}>
             <Table stickyHeader aria-label="sticky table">
-              <TableHead></TableHead>
+              <TableHead>
+                <TableRow>
+                  <TableCell align="right">Ducks</TableCell>
+                  <TableCell align="right">Food</TableCell>
+                  <TableCell align="right">Amount</TableCell>
+                  <TableCell align="right">Location</TableCell>
+                  <TableCell align="right">Date/Time</TableCell>
+                </TableRow>
+              </TableHead>
               <TableBody>
-                {duckData.map(({ _id, ...otherProps }) => (
-                  <DuckFeedingItem
-                    key={_id}
-                    _id={_id}
-                    align="right"
-                    {...otherProps}
-                  />
-                ))}
+                {duckData
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map(({ _id, ...otherProps }) => (
+                    <DuckFeedingItem
+                      key={_id}
+                      _id={_id}
+                      align="right"
+                      {...otherProps}
+                    />
+                  ))}
               </TableBody>
             </Table>
           </TableContainer>
         ) : (
           <h3>The database is empty, please add some entries.</h3>
         )}
+        <TablePagination
+          rowsPerPageOptions={[10, 25, 100]}
+          component="div"
+          count={duckData.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
       </Paper>
     </div>
   );
